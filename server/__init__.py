a89ea84e6a1741151f84b4ad78507d0170f5737f
Jan Stefański <janstefanskite@gmail.com>
2021-01-31 00:20:19 +0800

    wip
    working on pagination and proper links

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,5 @@
 import secrets
+from math import ceil
 
 from .services.player_info_fetch import fetch_player_info
 from flask import Flask, make_response, render_template, request, redirect, escape, cli
@@ -50,9 +51,9 @@ def load_lang(req: request):
 def index_page():
     idx_page = i18n_util.I18n('indexPage').load_translation(load_lang(request))
     navbar = i18n_util.I18n('navBar').load_translation(load_lang(request))
-    ranked_players = db_util.get_ranking()
+    ranked_players = db_util.get_ranking()[1]
     resp = make_response(
-        render_template('index.html', rankedPlayers=enumerate(ranked_players), indexPage=idx_page, navBar=navbar))
+        render_template('index.html', rankedPlayers=ranked_players, indexPage=idx_page, navBar=navbar))
     if request.args.get('lang'):
         resp.set_cookie('lang', request.args.get('lang'), secure=True, httponly=True, samesite='Strict')
     return resp
@@ -61,16 +62,21 @@ def index_page():
 @app.route("/leaderboards")
 def rankings_page():
     print(request.full_path)
-    allowed_limits = (10, 25, 50)
-    limit = int(request.args.get('limit') or 10) if int(request.args.get('limit') or 10) in allowed_limits else 10
-    page = request.args.get('page') or 1
+    print(request.args)
+    allowed_limits = (2, 10, 25, 50)
+    limit = int(request.args.get('limit') or 2) if int(request.args.get('limit') or 2) in allowed_limits else 2
+    page = request.args.get('page') or 0
     season = int(request.args.get('season') or 11)
     l_page = i18n_util.I18n('leaderboardsPage').load_translation(load_lang(request))
     navbar = i18n_util.I18n('navBar').load_translation(load_lang(request))
-    ranked_players = db_util.get_ranking(limit=limit, season=season)
+    ranking = db_util.get_ranking(limit=limit, season=season, page=page)
+    all_ranking_records = ranking[0]
+    pages = range(ceil(all_ranking_records / limit))
+    print(pages)
+    ranked_players = ranking[1]
     resp = make_response(
-        render_template('leaderboards.html', rankedPlayers=enumerate(ranked_players), leaderboardsPage=l_page,
-                        navBar=navbar))
+        render_template('leaderboards.html', rankedPlayers=ranked_players, leaderboardsPage=l_page,
+                        navBar=navbar, limit=limit, page=page, allowedLimits=allowed_limits))
     if request.args.get('lang'):
         resp.set_cookie('lang', request.args.get('lang'), secure=True, httponly=True, samesite='Strict')
     return resp
